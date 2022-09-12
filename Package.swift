5447893f54c58df96343b536ff7eba953d124d5c
Marius Seufzer <marius.seufzer@protonmail.com>
2022-09-12 09:17:49 +0200

    update Swift Package files

--- Package.swift
+++ Package.swift
@@ -1,9 +1,24 @@
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "VaporOAuth",
+    platforms: [
+       .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "OAuth",
+            targets: ["VaporOAuth"]
+        )
+    ],
     dependencies: [
-    	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-    	.Package(url: "https://github.com/vapor/auth-provider.git", majorVersion: 1),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
+    ],
+    targets: [
+        .target(
+            name: "VaporOAuth",
+            dependencies: [.product(name: "Vapor", package: "vapor")]
+        )
     ]
 )

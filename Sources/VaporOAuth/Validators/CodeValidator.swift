6e4ed870c2b69d873287c8732092ffe13a46ef20
Vamsi Madduluri <vamsi@dewonderstruck.com>
2024-01-10 15:32:42 +0530

    Add PKCEValidator for PKCE validation

--- Sources/VaporOAuth/Validators/CodeValidator.swift
+++ Sources/VaporOAuth/Validators/CodeValidator.swift
@@ -1,36 +1,24 @@
 import Foundation
-import Crypto // Import SwiftCrypto for SHA-256
+import Crypto
 
 struct CodeValidator {
     func validateCode(_ code: OAuthCode, clientID: String, redirectURI: String, codeVerifier: String?) -> Bool {
         guard code.clientID == clientID else {
             return false
         }
-
+        
         guard code.expiryDate >= Date() else {
             return false
         }
-
+        
         guard code.redirectURI == redirectURI else {
             return false
         }
-
-        // Optional PKCE validation
+        
         if let codeChallenge = code.codeChallenge, let codeChallengeMethod = code.codeChallengeMethod, let verifier = codeVerifier {
-            switch codeChallengeMethod {
-            case "S256":
-                // Transform the codeVerifier using SHA256 and base64-url-encode it
-                guard let verifierData = verifier.data(using: .utf8) else { return false }
-                let verifierHash = SHA256.hash(data: verifierData)
-                let encodedVerifier = Data(verifierHash).base64URLEncodedString()
-
-                return codeChallenge == encodedVerifier
-            default:
-                // If the code challenge method is unknown, fail the validation
-                return false
-            }
+            return PKCEValidator.validate(codeChallenge: codeChallenge, verifier: verifier, method: code.codeChallengeMethod)
         }
-
+        
         // If no PKCE was used (codeVerifier is nil), skip PKCE validation
         return true
     }

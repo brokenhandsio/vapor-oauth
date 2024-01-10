9788cb4d06dc3fadae9503bf8b72482291c2abb1
Vamsi Madduluri <vamsi@dewonderstruck.com>
2024-01-10 14:59:11 +0530

    Refactor: Updated StaticClientRetriever to work with concurrent access

--- Sources/VaporOAuth/DefaultImplementations/StaticClientRetriever.swift
+++ Sources/VaporOAuth/DefaultImplementations/StaticClientRetriever.swift
@@ -1,15 +1,13 @@
-import Vapor
-
-public actor StaticClientRetriever: ClientRetriever {
+public struct StaticClientRetriever: ClientRetriever {
     private let clients: [String: OAuthClient]
-
+    
     public init(clients: [OAuthClient]) {
         self.clients = clients.reduce(into: [String: OAuthClient]()) { (dict, client) in
             dict[client.clientID] = client
         }
     }
-
-    public func getClient(clientID: String) async throws -> OAuthClient? {
+    
+    public func getClient(clientID: String) throws -> OAuthClient? {
         return clients[clientID]
     }
 }
